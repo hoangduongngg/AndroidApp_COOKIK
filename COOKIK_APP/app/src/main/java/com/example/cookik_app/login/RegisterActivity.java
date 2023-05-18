40464c3a2a6569b64f31536b966a1c24370bfd56
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2023-05-19 01:26:52 +0700

    Login, SignIn with Firebase

---
+++ COOKIK_APP/app/src/main/java/com/example/cookik_app/login/RegisterActivity.java
@@ -0,0 +1,118 @@
+package com.example.cookik_app.login;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.cookik_app.R;
+import com.example.cookik_app.home.HomeActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText eGmail, ePass;
+    Button bt_signup;
+    TextView login_now;
+
+    FirebaseAuth mAuth;
+    String email, password;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        init_input();
+        init_firebase();
+        //Dang ky
+        bt_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = String.valueOf(eGmail.getText());
+                password = String.valueOf(ePass.getText());
+
+
+                //Neu de trong thi Toast thong bao
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(RegisterActivity.this, "Please enter email.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText( RegisterActivity.this, "Please enter password.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                create_user_firebase();
+            }
+        });
+        //Link sang trang nhap nhap
+        login_now.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    private void init_input() {
+        eGmail = findViewById(R.id.gmail);
+        ePass = findViewById(R.id.password);
+        bt_signup = findViewById(R.id.bt_signup);
+        login_now = findViewById(R.id.login_now);
+    }
+
+    private void init_firebase() {
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void create_user_firebase() {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(RegisterActivity.this, "Account created.",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(RegisterActivity.this, "Sign up failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}

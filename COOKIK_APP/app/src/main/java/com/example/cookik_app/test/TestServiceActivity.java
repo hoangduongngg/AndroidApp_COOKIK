ea4279dc513c8ccff8d9734c56c1bd32cd376074
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2023-05-20 17:37:00 +0700

    Test call API Success

--- COOKIK_APP/app/src/main/java/com/example/cookik_app/test/TestServiceActivity.java
+++ COOKIK_APP/app/src/main/java/com/example/cookik_app/test/TestServiceActivity.java
@@ -28,40 +28,28 @@ public class TestServiceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test_service);
         text_service = findViewById(R.id.text_service);
         bt_testCallAPI = findViewById(R.id.bt_testCallAPI);
+        callAPI();
 
         bt_testCallAPI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
                     callAPI();
-
-                    if (product!=null) {
-                        text_service.setText(product.getName());
-                    }
-                    else {
-                        text_service.setText("product null roi ban oi");
-
-                    }
-
                 } catch (Exception e) {
                     System.out.println(e);
                     Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-//        text_service.setText("product.getName()");
-//        text_service.setText(product.getName());
-//        Toast.makeText(this, responseBody, Toast.LENGTH_SHORT).show();
     }
     private void callAPI() {
-//        Product product = new Product();
         APIService.apiService.getProductById(3).enqueue(new Callback<Product>() {
             @Override
             public void onResponse(Call<Product> call, Response<Product> response) {
                 Toast.makeText(getApplicationContext(), "Call API Success", Toast.LENGTH_SHORT).show();
                 if (response.body()!= null) {
                     product = response.body();
+                    text_service.setText(product.getName());
                 }
             }
 
@@ -69,8 +57,6 @@ public class TestServiceActivity extends AppCompatActivity {
             public void onFailure(Call<Product> call, Throwable t) {
                 Toast.makeText(getApplicationContext(), "Call API Error", Toast.LENGTH_SHORT).show();
                 Toast.makeText(getApplicationContext(), t.toString(), Toast.LENGTH_LONG).show();
-                System.out.println(t.toString());
-
             }
         });
     }
